f242d25e05c4166af943a48e3331fd9c74da5765
root <root@vpn.bankai.ch>
2019-08-22 23:58:05 +0200

    fix wg service with fw rules

--- scripts/wg_service.sh
+++ scripts/wg_service.sh
@@ -8,3 +8,7 @@ ip link add wg0 type wireguard
 ip addr add 10.99.97.1/24 dev wg0
 wg setconf wg0 /etc/wireguard/server/server.conf
 ip link set wg0 up
+
+/sbin/iptables -t nat --append POSTROUTING -s 10.99.97.0/24 -j MASQUERADE
+/sbin/iptables --append FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+/sbin/iptables --append FORWARD -s 10.99.97.0/24 -j ACCEPT
